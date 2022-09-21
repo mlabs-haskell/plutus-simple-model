76496b17db859ce7cc0f9b3c4cefc01aad8386a4
Las Safin <me@las.rs>
2022-09-21 23:19:26 +0000

    stuff

--- flake.nix
+++ flake.nix
@@ -1,12 +1,22 @@
 {
   description = "plutus-simple-model";
-  nixConfig.bash-prompt = "\\[\\e[0m\\][\\[\\e[0;2m\\]nix \\[\\e[0;1m\\]psm \\[\\e[0;93m\\]\\w\\[\\e[0m\\]]\\[\\e[0m\\]$ \\[\\e[0m\\]";
-
-  inputs = {
-    tooling.url = "github:mlabs-haskell/mlabs-tooling.nix";
+  nixConfig = {
+    # We don't use Recursive Nix yet.
+    extra-experimental-features = [ "nix-command" "flakes" "ca-derivations" "recursive-nix" ];
+    extra-substituters = ["https://cache.iog.io" "https://public-plutonomicon.cachix.org" "https://mlabs.cachix.org"];
+    extra-trusted-public-keys = ["hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ=" "public-plutonomicon.cachix.org-1:3AKJMhCLn32gri1drGuaZmFrmnue+KkKrhhubQk/CWc="];
+    allow-import-from-derivation = "true";
+    bash-prompt = "\\[\\e[0m\\][\\[\\e[0;2m\\]nix \\[\\e[0;1m\\]psm \\[\\e[0;93m\\]\\w\\[\\e[0m\\]]\\[\\e[0m\\]$ \\[\\e[0m\\]";
+    cores = "1";
+    max-jobs = "auto";
+    auto-optimise-store = "true";
   };
 
-  outputs = { self, tooling }: tooling.mkHaskellFlake1 {
+  inputs = {
+    tooling.url = "github:mlabs-haskell/mlabs-tooling.nix?ref=las/work";
+  };
+
+  outputs = { self, tooling, ... }: tooling.mkHaskellFlake1 {
     project = {
       src = ./.;
     };
