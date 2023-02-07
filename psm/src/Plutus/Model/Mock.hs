f3e31a0a61546704c625a7642cf7de565eccde89
Geometer1729 <16kuhnb@gmail.com>
2023-02-07 12:49:38 -0500

    split to v1 and v2 for defaultBabbage(Params)

--- psm/src/Plutus/Model/Mock.hs
+++ psm/src/Plutus/Model/Mock.hs
@@ -97,7 +97,8 @@ module Plutus.Model.Mock (
   -- * Blockchain config
   readMockConfig,
   defaultAlonzo,
-  defaultBabbage,
+  defaultBabbageV1,
+  defaultBabbageV2,
   defaultMockConfig,
   skipLimits,
   warnLimits,
