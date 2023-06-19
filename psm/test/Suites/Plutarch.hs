7a2331d499bf605c6d85c8fa22ff5b86081fea8d
Geometer1729 <16kuhnb@gmail.com>
2023-06-19 12:31:55 -0400

    minor cleanup

--- psm/test/Suites/Plutarch.hs
+++ psm/test/Suites/Plutarch.hs
@@ -8,7 +8,6 @@ import Prelude
 import Control.Monad (void)
 import Data.Default (def)
 import Data.Either (fromRight)
-import Plutarch (Config (..), TracingMode (..))
 import Plutarch.Api.V2 (PValidator)
 import Plutarch.Monadic qualified as P
 import Plutus.Model (DatumMode (..), Run (..), TypedValidator (..), adaValue, defaultBabbageV2, mustFail, newUser, payToKey, payToScript, sendTx, signTx, spend, spendScript, userSpend, utxoAt)
@@ -35,8 +34,12 @@ testValidator guess = do
   u1 <- newUser (adaValue 1_000_000)
   u2 <- newUser (adaValue 1_000_000)
   let amt = adaValue 100
-      datum = toBuiltin $ plift $ phexByteStr "961b6dd3ede3cb8ecbaacbd68de040cd78eb2ed5889130cceb4c49268ea4d506"
-  -- from 'echo -n aa | sha256sum'
+      -- magic string is from 'echo -n aa | sha256sum'
+      datum =
+        toBuiltin $
+          plift $
+            phexByteStr
+              "961b6dd3ede3cb8ecbaacbd68de040cd78eb2ed5889130cceb4c49268ea4d506"
   do
     -- initialize
     sp <- spend u1 amt
@@ -57,7 +60,7 @@ testValidator guess = do
 validator :: TypedValidator BuiltinByteString BuiltinByteString
 validator =
   fromRight (error "no validator") $
-    mkTypedValidatorPlutarch def {tracingMode = DoTracing} gameContractPlutarch
+    mkTypedValidatorPlutarch def gameContractPlutarch
 
 gameContractPlutarch :: ClosedTerm PValidator
 gameContractPlutarch = plam $ \d' r' _ -> popaque $ P.do
